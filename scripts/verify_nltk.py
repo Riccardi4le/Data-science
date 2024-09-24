3e724827428c87d33fe573b3f448e5149dbeb890
Alessandro <Riccardi4le@gmail.com>
2024-09-24 23:05:31 +0200

    Add files via upload

---
+++ scripts/verify_nltk.py
@@ -0,0 +1,8 @@
+import nltk
+
+# Scarica le risorse di NLTK
+nltk.download('punkt')
+nltk.download('stopwords')
+
+# Verifica l'importazione
+print("NLTK è installato correttamente e le risorse sono state scaricate.")
